5e4221ad8fe655a0d82331236b84fef72ffb1eb3
Zeyneb Esendir <zeynebesendir@gmail.com>
2020-11-07 15:24:13 +0000

    Updates

---
+++ precache-manifest.33df21642c144e500147793a0a4765f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad0ad6bc5759937a71bdc8cf565b6a0",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "ecbb3153ca1829d4de41",
+    "url": "/crwn-clothing/static/css/main.761b030f.chunk.css"
+  },
+  {
+    "revision": "09c711b1a511546dd017",
+    "url": "/crwn-clothing/static/js/2.031c3c98.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-clothing/static/js/2.031c3c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbb3153ca1829d4de41",
+    "url": "/crwn-clothing/static/js/main.54a0050b.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
